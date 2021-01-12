a631dd76c3a10b0429c359cf4e16ccbf0fa71aae
jianie <77231186+jianie@users.noreply.github.com>
2021-01-13 01:42:40 +0900

    스택 미로

---
+++ Maze/Location2D.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct Location2D {
+	int  row;		
+	int  col;		
+	Location2D ( int r=0, int c=0 ) { row = r; col = c; }
+
+	bool isNeighbor( Location2D &p ) {
+		return( (row==p.row && (col==p.col-1 || col==p.col+1))
+			|| (col==p.col && (row==p.row-1 || row==p.row+1)) );
+	}
+	// 연산자 오버로딩 사용
+	bool operator==( Location2D &p ) { return row==p.row && col==p.col; }
+};
