9458d03d49b1519b07f666710e50666613551ba4
jianie <77231186+jianie@users.noreply.github.com>
2021-01-13 01:45:47 +0900

    Add files via upload

---
+++ LinkedQueue/05장-LinkedQueue.cpp
@@ -0,0 +1,14 @@
+// 05¿Â-LinkedQueueMain.cpp
+#include "LinkedQueue.h"
+
+void main()
+{
+	LinkedQueue que;
+	for( int i=1 ; i<10 ; i++ )
+		que.enqueue( new Node(i) );
+	que.display();
+	delete que.dequeue();
+	delete que.dequeue();
+	delete que.dequeue();
+	que.display();
+}
