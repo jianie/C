229612a6f8ea2c8250a4d576c2fe336d19de38d9
jianie <77231186+jianie@users.noreply.github.com>
2021-01-13 02:15:32 +0900

    인접행렬그래프

---
+++ AdjListGraph/11장-AdjListGraph.cpp
@@ -0,0 +1,22 @@
+#include "AdjListGraph.h"
+
+void main()
+{
+	AdjListGraph g;
+
+	//g.load("graph.txt");
+	for (int i = 0; i < 4; i++)
+		g.insertVertex('A' + i);	// 정점 삽입: 'A' 'B', ...
+	
+	g.insertEdge(0, 1);		// 간선 삽입
+	g.insertEdge(0, 3);
+	g.insertEdge(1, 2);
+	g.insertEdge(1, 3);
+	g.insertEdge(2, 3);
+
+
+
+	printf("인접 리스트로 표현한 그래프\n");
+	g.display();
+
+}
